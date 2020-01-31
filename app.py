b11b20e09bfb095480aac9e7d2555cfedd846da6
rajeevteejwal <43966279+rajeevteejwal@users.noreply.github.com>
2020-01-31 17:27:43 +0530

    Add digit recognizer

--- app.py
+++ app.py
@@ -1,14 +1,55 @@
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
+import base64
+from PIL import Image
+from io import BytesIO
+#from skimage.io import imsave, imread, imresize
+from keras.models import load_model
+#import tensorflow as tf
+#import re
+#import os
+#import sys
 
 app = Flask(__name__)
+#global cnn_model, graph, model
+
 model = pickle.load(open("model.pkl",'rb'))
+
+cnn_model = load_model('model.h5')
+#graph = tf.get_default_graph()
+
+'''def convertImage(imgData):
+    imgstr  = re.search(r'base64,(.*'.imgData1).group(1)
+    with open('output.png','wb') as output:
+        output.wite(imgstr.decode('base64'))'''
+
+def convertImage(imgData):
+    im = Image.open(BytesIO(base64.b64decode(imgData)))
+    im.save('output.png', 'PNG')
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/digit_recognize')
+def digit_recognition():
+    return render_template('digit_recognizer.html')
+
+@app.route('/predict',methods=['POST','GET'])
+def predict():
+    result = "5"
+    imgData = request.get_data()
+    convertImage(imgData)
+    '''x= imread('out.png',mode='L')
+    x=np.invert(x)
+    x = imresize(x,28,28)
+    x = x.reshape(1,28,28,1)
+    with graph.as_default():
+        out = cnn_model.predict(x)
+        result = np.array_str(np.argmax(out))'''
+    return result
 
 @app.route('/calculate', methods = ['POST'])
 def calculate_interest_rate():
